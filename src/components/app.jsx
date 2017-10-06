0ce5169683b2b6e6c987b168d78c9772e5c766c6
tomonakar <tomohisa.nak@gmail.com>
2017-10-06 20:23:53 +0900

    グーグルマップを導入

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import SearchForm from './SearchForm';
 import GeocodeResult from './GeocodeResult';
+import Map from './Map';
 
 const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
@@ -25,7 +26,6 @@ class App extends Component {
     axios
       .get(GEOCODE_ENDPOINT, { params: { address: place } })
       .then((results) => {
-        console.log(results);
         const data = results.data;
         const result = results.data.results[0];
         switch (data.status) {
@@ -61,6 +61,7 @@ class App extends Component {
           lat={this.state.lat}
           lng={this.state.lng}
         />
+        <Map lat={this.state.lat} lng={this.state.lng} />
       </div>
     );
   }
