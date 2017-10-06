bbec5ac8037e7b7087ec8dc207e3eb49322298c3
tomonakar <tomohisa.nak@gmail.com>
2017-10-06 16:32:29 +0900

    google apiを利用して住所と緯度経度を取得

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import SearchForm from './SearchForm';
 import GeocodeResult from './GeocodeResult';
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      place: '京タワー',
     };
   }
 
   handlePlaceSubmit(place) {
-    console.log(place);
+    axios
+      .get(GEOCODE_ENDPOINT, { params: { address: place } })
+      .then((results) => {
+        console.log(results);
+        const result = results.data.results[0];
+        const location = result.geometry.location;
+        this.setState({
+          address: result.formatted_address,
+          lat: location.lat,
+          lng: location.lng,
+         });
+
+      });
   }
 
   render() {
