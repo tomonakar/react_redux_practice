679a1962bc29ecf21947a055fbe0100135d366b4
tomonakar <tomohisa.nak@gmail.com>
2017-10-10 16:32:22 +0900

    表示項目を追加（2点間の距離など）

--- src/domain/HotelRepository.js
+++ src/domain/HotelRepository.js
@@ -1,3 +1,5 @@
+import geolib from 'geolib';
+
 import Rakuten from '../lib/Rakuten';
 
 const RAKUTEN_API_ID = '1005486963210399200';
@@ -13,11 +15,22 @@ export const searchHotelByLocation = (location) => {
   return Rakuten.Travel.simpleHotelSearch(params)
     .then(result =>
       result.data.hotels.map((hotel) => {
+        console.log(hotel);
         const basicInfo = hotel.hotel[0].hotelBasicInfo;
+        const price = basicInfo.hotelMinCharge;
+        const distance = geolib.getDistance(
+          { latitude: location.lat, longitude: location.lng },
+          { latitude: basicInfo.latitude, longitude: basicInfo.longitude },
+        );
         return {
           id: basicInfo.hotelNo,
           name: basicInfo.hotelName,
           url: basicInfo.hotelInformationUrl,
+          thumbUrl: basicInfo.hotelThumbnailUrl,
+          price: price ? `${price}円` : '空室無し',
+          reviewCount: basicInfo.reviewCount,
+          reviewAverage: basicInfo.reviewAverage,
+          distance,
         };
       }),
     );
