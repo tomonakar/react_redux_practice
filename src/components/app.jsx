4b05d3e1d5226ae66623e2d7c68d97c1d380e7b2
tomonakar <tomohisa.nak@gmail.com>
2017-10-11 10:54:16 +0900

    左ナビの追加

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,6 +5,10 @@ import SearchPage from './SearchPage';
 const App = () =>
   (
     <div className="app">
+      <ul className="left-navi">
+        <li><a href="/">ホテル検索</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
       <SearchPage />
     </div>
   );
