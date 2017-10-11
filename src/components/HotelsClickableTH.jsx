7a4dfc59569bc421f14a563a5ec6cd2be2ba64ab
tomonakar <tomohisa.nak@gmail.com>
2017-10-11 09:59:03 +0900

    ソート機能をコンポーネント化

--- src/components/HotelsClickableTH.jsx
+++ src/components/HotelsClickableTH.jsx
@@ -1,37 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import HotelRow from './HotelRow';
-
-const HotelsTable = ({ hotels, sortKey, onSort }) => (
-  <table>
-    <tbody>
-      <tr>
-        <th>画像</th>
-        <th>ホテル名</th>
-        <th onClick={() => onSort('price')} className="hotel-price-column">
-          値段{sortKey === 'price' ? ' ▲' : ''}
-        </th>
-        <th onClick={() => onSort('reviewAverage')} className="hotel-reviewAverage-column">
-          レビュー{sortKey === 'reviewAverage' ? ' ▲' : ''}
-        </th>
-        <th className="hotel-reviewCount-column">レビュー件数</th>
-        <th>距離</th>
-      </tr>
-      {hotels.map(hotel => (<HotelRow key={hotel.id} hotel={hotel} />))}
-    </tbody>
-  </table>
+const HotelsClickableTh = ({ label, sortKey, isSelected, onSort }) => (
+  <th
+    className="hotels-clickable-th"
+    onClick={() => onSort(sortKey)}
+  >
+    {label}{isSelected ? ' ▲' : ''}
+  </th>
 );
 
-HotelsTable.propTypes = {
-  hotels: PropTypes.arrayOf(PropTypes.any),
+HotelsClickableTh.propTypes = {
+  label: PropTypes.string.isRequired,
   sortKey: PropTypes.string.isRequired,
+  isSelected: PropTypes.bool.isRequired,
   onSort: PropTypes.func.isRequired,
 };
 
-HotelsTable.defaultProps = {
-  hotels: [],
-};
-
-
-export default HotelsTable;
+export default HotelsClickableTh;
--- src/components/HotelsTable.jsx
+++ src/components/HotelsTable.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import HotelRow from './HotelRow';
+import HotelsClickableTh from './HotelsClickableTh';
 
 const HotelsTable = ({ hotels, sortKey, onSort }) => (
   <table>
@@ -9,12 +10,18 @@ const HotelsTable = ({ hotels, sortKey, onSort }) => (
       <tr>
         <th>画像</th>
         <th>ホテル名</th>
-        <th onClick={() => onSort('price')} className="hotel-price-column">
-          値段{sortKey === 'price' ? ' ▲' : ''}
-        </th>
-        <th onClick={() => onSort('reviewAverage')} className="hotel-reviewAverage-column">
-          レビュー{sortKey === 'reviewAverage' ? ' ▲' : ''}
-        </th>
+        <HotelsClickableTh
+          label="値段"
+          sortKey="price"
+          isSelected={sortKey === 'price'}
+          onSort={key => onSort(key)}
+        />
+        <HotelsClickableTh
+          label="レビュー"
+          sortKey="reviewAverage"
+          isSelected={sortKey === 'reviewAverage'}
+          onSort={key => onSort(key)}
+        />
         <th className="hotel-reviewCount-column">レビュー件数</th>
         <th>距離</th>
       </tr>
