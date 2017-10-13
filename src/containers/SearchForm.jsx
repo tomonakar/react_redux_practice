31d8ee859f4e86f2e30df129c01d68e1c05e778f
tomonakar <tomohisa.nak@gmail.com>
2017-10-13 14:21:37 +0900

    Providerとconnectの実装

--- src/containers/SearchForm.jsx
+++ src/containers/SearchForm.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const mapStateTopProps = state => ({
+  place: state.place,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onPlaceChange: place => dispatch({ type: 'CHANGE_PLACE', place }),
+});
 
 const SearchForm = props => (
   <form className="search-form" onSubmit={e => props.onSubmit(e)}>
@@ -8,7 +17,10 @@ const SearchForm = props => (
       type="text"
       size="30"
       value={props.place}
-      onChange={e => props.onPlaceChange(e)}
+      onChange={(e) => {
+        e.preventDefault();
+        props.onPlaceChange(e.target.value);
+      }}
     />
     <input className="submit-button" type="submit" value="検索" />
   </form>
@@ -20,4 +32,8 @@ SearchForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
 
-export default SearchForm;
+
+const ConnectedSearchForm =
+  connect(mapStateTopProps, mapDispatchToProps)(SearchForm);
+
+export default ConnectedSearchForm;
--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import PropTypes from 'prop-types';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 // import App from './components/App';
 import SearchPage from './components/SearchPage';
 import reducer from './reducers/';
 
 ReactDOM.render(
-  <SearchPage
-    history={history}
-    location={location}
-    store={createStore(reducer)}
-  />,
+  <Provider store={createStore(reducer)}>
+    <SearchPage
+      history={history}
+      location={location}
+    />
+  </Provider>,
   document.querySelector('.container'),
 );
