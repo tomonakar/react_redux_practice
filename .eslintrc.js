c520336a21f839093eda51017306f0e3139c8a93
tomonakar <tomohisa.nak@gmail.com>
2017-10-13 14:50:27 +0900

    redux-devtoolsを導入

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,10 @@ module.exports = {
   "extends": "airbnb",
   "env": {
     "browser": true
+  },
+  "rules": {
+    "no-underscore-dangle" : [
+      "error",
+      {"allow": ["__REDUX_DEVTOOLS_EXTENSION__"] }]
   }
 };
